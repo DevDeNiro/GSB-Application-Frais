c04b35c53a36fb10e8113d0ff2238822216b134b
Noodles-boop <Oldoni.jeremy@outlook.fr>
2021-12-22 19:55:03 +0100

    ...

--- src/Form/VehiculesType.php
+++ src/Form/VehiculesType.php
@@ -16,63 +16,71 @@ class VehiculesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('marque', TextType::class ,[
-                'label' => 'Marque',
-                'required' => true,
-                'attr' => array ( 
-                    'placeholder' => 'Marque' )
-            ],
-
-            ChoiceType::class, [
-
+        ->add('marque', ChoiceType::class, [
                 'choices'  => [
-                    'Maybe' => 'Maybe',
-                    'Yes' => 'Yes',
-                    'No' => 'Qsd',
+
+                    'Sélectionner un véhicule' => '',
+ 
+                    'Marque de voitures' => [
+                        'Tesla' => 'Tesla',
+                        'Renauld' => 'Renauld',
+                        'Peugeot' => 'Peugeot',
+                        'Toyota' => 'Toyota',
+                        'mitsubishi' => 'mitsubishi',
+                        'honda' => 'honda',
+                        'bmw' => 'bmw',
+                        'mercedes' => 'mercedes',
+                        'Volkswagen' => 'Volkswagen',
+                        'audi' => 'audi',
+                        // 'autre' => 'autre', Autre à pouvoir renseigner librement          
                 ],
-            ])
+            ]
+        ])
             
             ->add('modele', TextType::class ,[
-                'label' => 'Modèle',
+               
                 'required' => true,
                 'attr' => array ( 
                     'placeholder' => 'Modèle du véhicule' )
             ])
 
             ->add('immatriculation', TextType::class ,[
-                'label' => 'Immatriculation',
+                
                 'required' => true,
                 'attr' => array ( 
                     'placeholder' => 'AA-123-AA' )
             ])
 
-            ->add('carburant', TextType::class ,[
-                'label' => 'Carburant',
-                'required' => true,
-                'attr' => array ( 
-                    'placeholder' => 'Type de carburant' )
-            ])
+            ->add('carburant', ChoiceType::class, [
+                'choices'  => [
+
+                    'Sélectionner un carburant' => '',
+                   
+                    'Type de carburant' => [
+                        'Diesel' => 'Diesel',
+                        'Essence' => 'Essence',
+                        'GNV' => 'GNV',
+                        'GPL' => 'GPL',
+                        // 'autre' => 'autre', Autre à pouvoir renseigner librement          
+                ],
+            ]
+        ])
 
             ->add('chevaux_fiscaux', NumberType::class ,[
-                'label' => ' Chevaux fiscaux',
+               
                 'required' => true,
                 'attr' => array ( 
                     'placeholder' => 'Chevaux fiscaux' )
             ])
 
             ->add('annee', NumberType::class ,[
-                'label' => 'Année',
+               
                 'required' => true, 
                 'attr' => array ( 
                     'placeholder' => 'Année du véhicule' )           
             ])
 
-            // ->add('submit', SubmitType::class, [ 
-                
-            //     'label' => 'Envoyer',
-            //     // 'row_attr' => [ 'class' => 'btn btn-outline-success mt-3 ', 
-            // ])
-             
+            
             ->getForm()
         ;
     }
