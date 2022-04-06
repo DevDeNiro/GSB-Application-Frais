d7b6c4d8a39ec856817cb4eae1294a1577317902
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2022-04-06 11:31:19 +0200

    composer update

--- src/Controller/login.php
+++ src/Controller/login.php
@@ -8,6 +8,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
+use App\Entity\User;
+use App\Entity\FraisForfait;
+use App\Entity\FraisHorsForfait;
+use App\Entity\Vehicule;
+
+
 class login extends AbstractController
 {
     /**
@@ -16,7 +22,18 @@ class login extends AbstractController
     */
 
     public function test(Request $request) : Response // Fonction qui permet d'ajouter un nouveau matériel dans la base de données via un formulaire
-    {       
+    {      
+        
+        
+        // $setUser = new User();
+        // $setUser->setNom('Folder');
+        // $setUser->setGroupementPublic($groupementPublic);
+
+ 
+        // $entityManager = $doctrine->getManager();
+        // $entityManager->persist($setUser);
+        // $entityManager->flush();
+
         return $this->render('login/login.html.twig',[ // Création du formulaire par symfony   
         
         ]);
