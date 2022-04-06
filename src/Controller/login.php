6b102875fcd2c94e6e0e1e27a4d811901d9cc358
Noodles-boop <72260596+Noodles-boop@users.noreply.github.com>
2022-04-06 11:48:05 +0200

    setters user

--- src/Controller/login.php
+++ src/Controller/login.php
@@ -7,6 +7,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Doctrine\Persistence\ManagerRegistry;
+
 
 use App\Entity\User;
 use App\Entity\FraisForfait;
@@ -21,18 +23,23 @@ class login extends AbstractController
      * @Method({"GET"})
     */
 
-    public function test(Request $request) : Response // Fonction qui permet d'ajouter un nouveau matériel dans la base de données via un formulaire
+    public function test(Request $request, ManagerRegistry $doctrine) : Response // Fonction qui permet d'ajouter un nouveau matériel dans la base de données via un formulaire
     {      
-        
-        
-        // $setUser = new User();
-        // $setUser->setNom('Folder');
-        // $setUser->setGroupementPublic($groupementPublic);
-
+     
+        $setUser = new User();
+        $setUser->setUsername('dandre');
+        $setUser->setRoles('["ROLE_VISITEUR"]');
+        $setUser->setPassword('$argon2id$v=19$m=65536,t=4,p=1$cDBFcTB0RVViMVNxTGFFQQ$51JuUEGxfdnoKkEPaHcs01EBsUNxxdGVAgaaq/7+22k');
+        $setUser->setNom('Andre');
+        $setUser->setPrenom('David');
+        $setUser->setCp(46200);
+        $setUser->setVille('Lalbenque');
+        $setUser->setDateEmbauche('1998-11-23');
+        $setUser->setAdresse('1 rue Petit');
  
-        // $entityManager = $doctrine->getManager();
-        // $entityManager->persist($setUser);
-        // $entityManager->flush();
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($setUser);
+        $entityManager->flush();
 
         return $this->render('login/login.html.twig',[ // Création du formulaire par symfony   
         
